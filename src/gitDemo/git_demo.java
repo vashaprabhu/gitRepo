fcefec04850c9f819a42a5d6af1e95d9687e5a2d
VarshaVar <varshaks14@gmail.com>
2020-04-14 18:40:14 -0700

    commiting master

---
+++ src/gitDemo/git_demo.java
@@ -0,0 +1,16 @@
+package gitDemo;
+
+public class git_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//This is git demo
+		//I'm practicing git commands which we use everday in n out
+		//im in subBranch of master
+		// now i'm in subbranch 
+		// let me see how it works on pull, fetch, merge, commit and push --- line 12
+
+	}
+
+}
